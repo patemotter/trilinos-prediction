d4aeba189094344183665e2a20a1d88b370ba3c6
Pate Motter <patemotter@gmail.com>
2017-01-19 14:01:47 -0600

    Seems to be working on the Pandas side of things

--- test/main.py
+++ test/main.py
@@ -19,8 +19,7 @@ combined = pd.merge(timings, properties, on='matrix')
 combined['solver_num'] = combined.solver.map(
     {'FIXED_POINT': 0, 'BICGSTAB': 1, 'MINRES': 2, 'PSEUDOBLOCK_CG': 3, 'PSEUDOBLOCK_STOCHASTIC_CG': 4,
      'PSEUDOBLOCK_TFQMR': 5, 'TFQMR': 6, 'LSQR': 7, 'PSEUDOBLOCK_GMRES': 8}).astype(int)
-combined['prec_num'] = combined.prec.map({'ILUT': 0, 'RILUK': 1, 'RELAXATION': 2, 'CHEBYSHEV': 3, 'NONE': 4}).astype(
-    int)
+combined['prec_num'] = combined.prec.map({'ILUT': 0, 'RILUK': 1, 'RELAXATION': 2, 'CHEBYSHEV': 3, 'NONE': 4}).astype(int)
 combined['status_num'] = combined.status.map({'error': -1, 'unconverged': 0, 'converged': 1}).astype(int)
 
 grouped = combined.groupby(['matrix', 'status_num'])
@@ -30,18 +29,38 @@ matrix_best_times = grouped['time'].aggregate(np.min)
 
 # print(matrix_best_times["1138_bus.mtx"][1])
 
-
+good_bad_series = []
+new_time_series = []
 for index, row in combined.iterrows(): #iterates through the datframe
     #for mat in matrix_best_times.keys():
     current_matrix_time = row['time']
     matrix_name = row['matrix']
-    matrix_min_time = matrix_best_times[matrix_name][1] # 1 indicates converged
-    if current_matrix_time == matrix_min_time: # check if current time matches best time for that matrix
-        print(matrix_best_times[matrix_name][1])
+    try:
+        matrix_min_time = matrix_best_times[matrix_name][1] # 1 indicates converged
+    except:
+        matrix_min_time = np.inf
+        #print(matrix_name, "exception")
 
+#    if current_matrix_time == matrix_min_time: # check if current time matches best time for that matrix
+    if row['status_num'] != 1:
+        good_bad_series.append("bad")
+        new_time_series.append(np.inf)
+
+        #print(matrix_name, "bad")
+    elif current_matrix_time <= 1.25*matrix_min_time:
+        good_bad_series.append("good")
+        new_time_series.append(current_matrix_time)
+        #print(matrix_name, "good")
+
+a = pd.Series(good_bad_series)
+a.to_csv('good_bad_list.csv')
+b = pd.Series(new_time_series)
+b.to_csv('new_time_list.csv')
+
+#print(good_bad_series)
+combined = combined.assign(good_or_bad = pd.Series(good_bad_series))
+combined.to_csv('hope.csv')
     # todo add in fraction of min
     # todo set error/converged to inf times
     # todo compare only against converged items
     # todo some error checking in case no items converged for that matrix
-
-
