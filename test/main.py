64d1160dcccf698b67295184d7ee10624d7f9464
Pate Motter <patemotter@gmail.com>
2017-01-20 12:31:10 -0600

    Some small cleanup

--- test/main.py
+++ test/main.py
@@ -21,17 +21,19 @@ np8_timings = pd.read_csv('../data/np8_results.csv', header=0)
 np10_timings = pd.read_csv('../data/np10_results.csv', header=0)
 np12_timings = pd.read_csv('../data/np12_results.csv', header=0)
 
+# Make a list of all the individual np dataframes and combine them
 timings = [np1_timings, np2_timings, np4_timings, np6_timings, np8_timings, np10_timings, np12_timings]
 all_timing_data = pd.concat(timings)
+all_timing_data.columns = ['np', 'matrix', 'solver', 'prec', 'status', 'time', 'iters', 'resid']
+
+# Make a hashtable for storing matrix names as integers
 matrix_names = all_timing_data['matrix_name'].unique()
 matrix_list = {}
 for name in matrix_names:
     matrix_list[hash(name)] = name
-    a = pd.Series(matrix_list)
+    matrix_series = pd.Series(matrix_list)
+matrix_series.to_csv('hashed.csv')
 
-a.to_csv('hashed.csv')
-
-all_timing_data.columns = ['np', 'matrix', 'solver', 'prec', 'status', 'time', 'iters', 'resid']
 
 # Read in the matrix properties from the csv file, aliases cols
 properties = pd.read_csv('../data/uflorida-features.csv', header=0)
@@ -45,9 +47,8 @@ properties.columns = ['rows', 'cols', 'min_nnz_row', 'row_var', 'col_var',
                       'diag_sign', 'diag_nnz', 'lower_bw', 'upper_bw', 'row_log_val_spread',
                       'col_log_val_spread', 'symm', 'matrix']
 
-# Combine the two dataframes (timings and properties)
+# Combine the two dataframes (timings and properties) based on matrix name
 combined = pd.merge(all_timing_data, properties, on='matrix')
-print(len(combined))
 
 # Change string entries into numerical (for SKLearn)
 combined['solver_num'] = combined.solver.map(
@@ -66,14 +67,8 @@ good_bad_list = []
 new_time_list = []
 hash_list = []
 
-total_count = 0
-count_0 = 0
-count_1 = 0
-count_2 = 0
-count_3 = 0
 # Iterate through each row of the dataframe
 for index, row in combined.iterrows():
-    total_count += 1
     current_matrix_time = row['time']
     matrix_name = row['matrix']
     hash_list.append(hash(matrix_name))
@@ -83,27 +78,21 @@ for index, row in combined.iterrows():
         matrix_min_time = matrix_best_times[matrix_name][1]  # 1 indicates converged
     except:
         matrix_min_time = np.inf
-        count_0 += 1
 
     # Error or unconverged runs = inf time
     if row['status_num'] != 1:
         good_bad_list.append(-1)
         new_time_list.append(np.inf)
-        count_1 += 1
     # Good = anything within 25% of the fastest run for that matrix
     elif current_matrix_time <= 1.25 * matrix_min_time:
         good_bad_list.append(1)
         new_time_list.append(current_matrix_time)
-        count_2 += 2
     # Bad = anything else outside of that range but still converged
     else:
         good_bad_list.append(-1)
         new_time_list.append(current_matrix_time)
-        count_3 += 3
 
-print(count_0, count_1, count_2, count_3, total_count)
-
-# Create Pandas series from the lists
+# Create Pandas series from the lists which used to contain strings
 good_bad_series = pd.Series(good_bad_list)
 good_bad_series.to_csv('good_bad_series.csv')
 combined = combined.assign(good_or_bad=pd.Series(good_bad_series))
@@ -117,6 +106,5 @@ name_hash_series.to_csv('hash_series.csv')
 combined = combined.assign(matrix_hash=pd.Series(name_hash_series))
 
 # Add series to dataframe as new columns
-# todo This is causing the main issue, need new way of outputting to files
-ready = combined[['np', 'matrix_hash', 'solver_num', 'prec_num', 'status_num', 'new_time', 'good_or_bad']]
-ready.to_csv('ready.csv')
+cleaned_timing_data = combined[['np', 'matrix_hash', 'solver_num', 'prec_num', 'status_num', 'new_time', 'good_or_bad']]
+cleaned_timing_data.to_csv('ready.csv')
