36126f5a228ee5b4f99ffa369386ccce244bef50
Pate Motter <patemotter@gmail.com>
2017-01-26 13:00:37 -0600

    Adding plotting of confusion matrix

--- ml_files/perform_sklearn.py
+++ ml_files/perform_sklearn.py
@@ -2,6 +2,7 @@
 # Pate Motter
 # 1-22-17
 
+import itertools
 import pandas as pd
 import numpy as np
 import random
@@ -21,6 +22,40 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 np.set_printoptions(precision=5)
 
@@ -40,6 +75,7 @@ rng = np.random.RandomState()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rng, stratify=y, test_size=0.5)
 cross_validation = StratifiedKFold(n_splits=3)
 
+"""
 # Based on tutorial 04 - Training and Testing Data
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(X_train, y_train)
@@ -74,11 +110,16 @@ print("Decision Tree Training Score:", classifier.score(X_train, y_train))
 print("Decision Tree Test Score:", classifier.score(X_test, y_test))
 print(confusion_matrix(y_test, y_test_pred))
 print(classification_report(y_test, y_test_pred))
-
+"""
 classifier = GradientBoostingClassifier()
 classifier.fit(X_train, y_train)
 y_test_pred = classifier.predict(X_test)
 print("Gradient Boosting Training Score:", classifier.score(X_train, y_train))
 print("Gradient Boosting Test Score:", classifier.score(X_test, y_test))
 print(confusion_matrix(y_test, y_test_pred))
-print(classification_report(y_test, y_test_pred))
+#print(classification_report(y_test, y_test_pred))
+plt.figure()
+cnf_matrix = confusion_matrix(y_test, y_test_pred)
+plot_confusion_matrix(cnf_matrix, classes=['-1', '1'], normalize=False,
+                      title='Normalized confusion matrix')
+plt.show()
